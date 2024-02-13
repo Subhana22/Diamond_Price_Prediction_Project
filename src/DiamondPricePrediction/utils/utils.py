15fd142a6b35e8aa0f704ac9d2b5403e6a7224b4
Subhana22 <subhanasheikh22@gmail.com>
2024-02-13 18:14:06 +0530

    prediction completed

--- src/DiamondPricePrediction/utils/utils.py
+++ src/DiamondPricePrediction/utils/utils.py
@@ -35,7 +35,8 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
         return report
     except Exception as e:
         logging.info("Exception occured during model training ")  
-        raise customexception(e,sys)  
+        raise customexception(e,sys) 
+     
     def load_object(file_path):
         try:
             with open(file_path,'rb') as file_obj:
--- test.py
+++ test.py
@@ -1,11 +1,16 @@
-import os
 
-path="notebooks/research.ipynb"
+
+'''path="notebooks/research.ipynb"
 
 dir,file=os.path.split(path)
 
 os.makedirs(dir,exist_ok=True)
 
 with open(file,"w") as f:
-    pass
-    
+    pass'''
+
+from src.DiamondPricePrediction.pipelines.prediction_pipeline import CustomData
+
+custdataobj=CustomData(1,0.3,62.1,58,4.27,4.29,2.66,499,"Ideal","E","SI1")
+data=custdataobj.get_data_as_dataframe()
+print(data)
